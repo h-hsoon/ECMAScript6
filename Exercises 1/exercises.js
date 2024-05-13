be6ae29912438b2a56cce511c9e6e81ac308d68c
unknown <hannahsoon1996@gmail.com>
2024-05-13 21:18:56 +0200

    Exercise 3

--- Exercises 1/exercises.js
+++ Exercises 1/exercises.js
@@ -9,3 +9,11 @@ let isAdmin = user.isAdmin ? true : false;
 // Exercise 2
 let planetName = 'earth' ;
 let currentVisitor = 'Hanna';
+
+// Exercise 3
+let user1 = {};
+user1.name = 'John';
+user1.surName = 'Smith';
+user.name = "Pete";
+delete user.name;
+
