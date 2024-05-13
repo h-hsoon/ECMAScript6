a70d5fc18cb746f8b1900c4b490a70405dcabc8c
unknown <hannahsoon1996@gmail.com>
2024-05-13 21:29:21 +0200

    Exercise 3 update and Exercise 4

--- Exercises 1/exercises.js
+++ Exercises 1/exercises.js
@@ -11,9 +11,20 @@ let planetName = 'earth' ;
 let currentVisitor = 'Hanna';
 
 // Exercise 3
+let phrase = "Hello"
+
+if (true) {
+  let user = "John";
+  function sayHi() {
+    alert(`${phrase}, ${user}`)
+  }
+}
+
+sayHi(); // it's an alert Hello John because of the function
+
+// Exercise 4
 let user1 = {};
 user1.name = 'John';
 user1.surName = 'Smith';
 user.name = "Pete";
 delete user.name;
-
